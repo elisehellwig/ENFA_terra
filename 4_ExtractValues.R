1c41791a4f89bad9a2b23a48749b76963dd0f443
Elise Hellwig <echellwig@ucdavis.edu>
2023-09-15 21:33:56 -0700

    rewritten with terra and vectorization.

--- 4_ExtractValues.R
+++ 4_ExtractValues.R
@@ -1,105 +1,131 @@
 #Marmot Data
-setwd("/Volumes/AvivasDissertation_2018_4158472461/Dissertation/GISData_Jan2018UpdatedNDVI") 
+setwd("/Users/elisehellwig/Library/CloudStorage/GoogleDrive-echellwig@ucdavis.edu/Shared drives/Alpine Mammals Updated/GISData_Jan2018UpdatedNDVI")
+outpath <- "/Users/elisehellwig/Library/CloudStorage/GoogleDrive-echellwig@ucdavis.edu/My Drive/Transfer/Data"
 
-library(rgdal)
-library(raster)
 
-var.names <- c("Elevation30SNV", "Slope30SNV", "TRI30SNV", "NDVICVMeanMax1989_2015SNV",
-               "NDVIMeanMax1989_2015SNV", "Precip0609SNV",  "Precip1005SNV", "Tmin01",
-               "Tmax07SNV", "SnowFreeDaysSNV", "Aspen30m", "Conifer30m","Meadow30m",
-               "Mixed30m", "Rock30m", "Shrub30m") 
+library(terra)
+library(data.table)
 
-#Add Random Points (unused habitat) generated earlier.  50,000 random points 500m from transects. 
-RandPtsDF <- shapefile("Analysis/RandomPoints/RandPtsTransect500mBuffer.shp")
-plot(RandPtsDF)
+env_data <- rast(file.path(outpath,"EnvironmentData.GTiff"))
 
-#Get Marmot Data
-Marfla <- shapefile("Locations/Transects/Marfla/MarflaCoordsSNV.shp")
-class(Marfla)
+#Add Random Points (available habitat) generated earlier.  50,000 random points 500m from transects. 
+rand_pts <- vect('Updated_Data/SVN_Random_Points.geojson')
 
-#Extract values for MarmotStack layers at Random points
-#This is going to take a long time with the 50,000 points
-#Used Points generated within a 500mBuffer
-#Available500mBuff <-extract(MarmotStack, RandPtsDF)
-str(Available500mBuff)
-head(Available500mBuff)
-class(Available500mBuff)
+rodent_abrevs <- c("Marfla", "Spelat", "Spebel", "Pika")
 
-#This takes 10 minutes, so use the csv you made last time.
-#write.csv(Available500mBuff, "SNV/Available500mBuff", row.names = FALSE)
-Available500mBuff <- read.csv("SNV/Available500mBuff") 
+rodent_fns <- sapply(rodent_abrevs, function(sciname) {
+  paste0("Locations/Transects/", sciname, "/", sciname, "CoordsSNV.shp")
+}) 
 
-#There is a problem combining the available (Available500mBuff) and the used(Marflahabvars).  I think it is because the Available is a matrix, the used is a data.frame, and I haven't added the coordinates like I did for the used - resulting in different column numbers.  So attempting to use the code to add UTM to the available.
+rodent_pts_ls <- lapply(1:length(rodent_abrevs), function(i) {
+  sv <- vect(rodent_fns[i])
+  sv$Species <- rodent_abrevs[i]
+  sv
+})
 
-Available500mBuff.dfm <- data.frame(Available500mBuff)
-str(Available500mBuff.dfm)
-colnames(Available500mBuff.dfm) <- var.names
-str(Available500mBuff.dfm)
+rodent_pts <- vect(rodent_pts_ls)
 
-Avail.coords <- data.frame(coordinates(RandPtsDF))
-colnames(Avail.coords) <- c("UTMEW", "UTMNS")
-str(Avail.coords)
-nrow(unique(Avail.coords))
+rodent_pts$PA <- 1 #presence/available as 1 for present
 
-habvarsAvail500mBuff <- cbind(Avail.coords, Available500mBuff.dfm)
-str(habvarsAvail500mBuff)
+all_pts <- rbind(rodent_pts[,c('Species', 'PA')], rand_pts)
 
-#Sets These points as available (not present/observed)
-habvarsAvail500mBuff$Marfla <- 0
+all_pts$ID <- 1:nrow(all_pts)
 
-#The below Gets rid of observations below 2500.  This pulls out random points below there.  All transects are above this.  (around 15k points got pulled out)
-elevfilter <- habvarsAvail500mBuff$Elevation30SNV > 2500
-Marflahabvars.temp3elevfilt <- habvarsAvail500mBuff[elevfilter, ]
-str(Marflahabvars.temp3elevfilt)
+pa_env <- extract(env_data, all_pts)
 
-#Marflahabvars.temp3 <- Marflahabvars.temp3elevfilt // this variable name is defined below all of the data
-                                                    # not just the available data
+pa_sv <- merge(all_pts, pa_env, by='ID')
 
-#Extract values for MarmotStack layers at Marfla points (this takes 10min, skip to code below to read in csv you made last time)
-#HabitatVariablesMarfla <- extract(MarmotStack, Marfla)
-#class(HabitatVariablesMarfla)
+pa0 <- data.frame(pa_sv)
+setDT(pa0)
 
-#write.csv(HabitatVariablesMarfla, "SNV/HabitatVariablesMarfla", row.names = FALSE)
-HabitatVariablesMarfla <- read.csv("SNV/HabitatVariablesMarfla")
+pa <- pa0[Elevation>2500]
 
-#Need it in a data frame format
-HabitatVariablesMarfla.dfm <- data.frame(HabitatVariablesMarfla)
-str(HabitatVariablesMarfla.dfm)
-colnames(HabitatVariablesMarfla.dfm) <- var.names
-str(HabitatVariablesMarfla.dfm)
-
-#Making it a spatial dataframe?
-marfla.coords <- data.frame(coordinates(Marfla))
-colnames(marfla.coords) <- c("UTMEW", "UTMNS")
-str(marfla.coords)
-nrow(unique(marfla.coords))
-
-#Combine Used Points and UTM into one data frame. 
-#habvarsMarfla <- cbind(marfla.coords, HabitatVariablesMarfla.dfm)
-#str(habvarsMarfla)
-
-#Get them into a CSV? 
-#write.csv(habvarsMarfla, file = "HabitatVariablesMarfla.csv", row.names = FALSE)
-habvarsMarfla <- read.csv("HabitatVariablesMarfla.csv")
-
-str(habvarsMarfla)
-class(habvarsMarfla)
-
-##HELP - The following 10 lines of script was inherited, and I'm not sure what the relevance is of the temporary name assignments.   
-Marflahabvars.temp <- read.csv("HabitatVariablesMarfla.csv", header=TRUE)
-str (Marflahabvars.temp)
-
-Marflahabvars.temp <- habvarsMarfla
-str (Marflahabvars.temp)
-
-Marflahabvars.temp2 <- Marflahabvars.temp
-str(Marflahabvars.temp2)
-###END HELP
-
-Marflahabvars.temp2$Marfla <- 1 # Setting the presence/available variable equal to presence (aka 1)
-
-# Combine used and available habitat 
-Marflahabvars.temp3 <- rbind(Marflahabvars.temp2, Marflahabvars.temp3elevfilt)
-str(Marflahabvars.temp3)
-
-write.csv(Marflahabvars.temp3, file = "Used_And_Avail_500mBuff_HabitatVariablesMarfla.csv", row.names = FALSE)
+fwrite(pa, file.path(outpath, 'PresenceAvailableData.csv'))
+# 
+# 
+# #Extract values for MarmotStack layers at Random points
+# #This is going to take a long time with the 50,000 points
+# #Used Points generated within a 500mBuffer
+# #Available500mBuff <-extract(MarmotStack, RandPtsDF)
+# str(Available500mBuff)
+# head(Available500mBuff)
+# class(Available500mBuff)
+# 
+# #This takes 10 minutes, so use the csv you made last time.
+# #write.csv(Available500mBuff, "SNV/Available500mBuff", row.names = FALSE)
+# Available500mBuff <- read.csv("SNV/Available500mBuff") 
+# 
+# #There is a problem combining the available (Available500mBuff) and the used(Marflahabvars).  I think it is because the Available is a matrix, the used is a data.frame, and I haven't added the coordinates like I did for the used - resulting in different column numbers.  So attempting to use the code to add UTM to the available.
+# 
+# Available500mBuff.dfm <- data.frame(Available500mBuff)
+# str(Available500mBuff.dfm)
+# colnames(Available500mBuff.dfm) <- var.names
+# str(Available500mBuff.dfm)
+# 
+# Avail.coords <- data.frame(coordinates(RandPtsDF))
+# colnames(Avail.coords) <- c("UTMEW", "UTMNS")
+# str(Avail.coords)
+# nrow(unique(Avail.coords))
+# 
+# habvarsAvail500mBuff <- cbind(Avail.coords, Available500mBuff.dfm)
+# str(habvarsAvail500mBuff)
+# 
+# #Sets These points as available (not present/observed)
+# habvarsAvail500mBuff$Marfla <- 0
+# 
+# #The below Gets rid of observations below 2500.  This pulls out random points below there.  All transects are above this.  (around 15k points got pulled out)
+# elevfilter <- habvarsAvail500mBuff$Elevation30SNV > 2500
+# Marflahabvars.temp3elevfilt <- habvarsAvail500mBuff[elevfilter, ]
+# str(Marflahabvars.temp3elevfilt)
+# 
+# #Marflahabvars.temp3 <- Marflahabvars.temp3elevfilt // this variable name is defined below all of the data
+#                                                     # not just the available data
+# 
+# #Extract values for MarmotStack layers at Marfla points (this takes 10min, skip to code below to read in csv you made last time)
+# #HabitatVariablesMarfla <- extract(MarmotStack, Marfla)
+# #class(HabitatVariablesMarfla)
+# 
+# #write.csv(HabitatVariablesMarfla, "SNV/HabitatVariablesMarfla", row.names = FALSE)
+# HabitatVariablesMarfla <- read.csv("SNV/HabitatVariablesMarfla")
+# 
+# #Need it in a data frame format
+# HabitatVariablesMarfla.dfm <- data.frame(HabitatVariablesMarfla)
+# str(HabitatVariablesMarfla.dfm)
+# colnames(HabitatVariablesMarfla.dfm) <- var.names
+# str(HabitatVariablesMarfla.dfm)
+# 
+# #Making it a spatial dataframe?
+# marfla.coords <- data.frame(coordinates(Marfla))
+# colnames(marfla.coords) <- c("UTMEW", "UTMNS")
+# str(marfla.coords)
+# nrow(unique(marfla.coords))
+# 
+# #Combine Used Points and UTM into one data frame. 
+# #habvarsMarfla <- cbind(marfla.coords, HabitatVariablesMarfla.dfm)
+# #str(habvarsMarfla)
+# 
+# #Get them into a CSV? 
+# #write.csv(habvarsMarfla, file = "HabitatVariablesMarfla.csv", row.names = FALSE)
+# habvarsMarfla <- read.csv("HabitatVariablesMarfla.csv")
+# 
+# str(habvarsMarfla)
+# class(habvarsMarfla)
+# 
+# ##HELP - The following 10 lines of script was inherited, and I'm not sure what the relevance is of the temporary name assignments.   
+# Marflahabvars.temp <- read.csv("HabitatVariablesMarfla.csv", header=TRUE)
+# str (Marflahabvars.temp)
+# 
+# Marflahabvars.temp <- habvarsMarfla
+# str (Marflahabvars.temp)
+# 
+# Marflahabvars.temp2 <- Marflahabvars.temp
+# str(Marflahabvars.temp2)
+# ###END HELP
+# 
+# Marflahabvars.temp2$Marfla <- 1 # Setting the presence/available variable equal to presence (aka 1)
+# 
+# # Combine used and available habitat 
+# Marflahabvars.temp3 <- rbind(Marflahabvars.temp2, Marflahabvars.temp3elevfilt)
+# str(Marflahabvars.temp3)
+# 
+# write.csv(Marflahabvars.temp3, file = "Used_And_Avail_500mBuff_HabitatVariablesMarfla.csv", row.names = FALSE)
